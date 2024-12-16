84955841c50e7aa0855d9bd8e35cdda60895289e
Jayvirsinh Chudasama <jayvirchudasama7@gmail.com>
2024-12-16 00:46:13 -0500

    auth using auth/devdodjo

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,21 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+// Route::get('/', [UserController::class, 'loadUserHome'])->middleware('user');
+Route::get('/admin', [AdminController::class, 'loadAdminHome'])->middleware('admin');
 
-Route::get('/register', function () {
-    return view('register');
-});
+// Route::get('/login', function () {
+//     return view('login');
+// });
+
+// Route::get('/register', function () {
+//     return view('register');
+// });
